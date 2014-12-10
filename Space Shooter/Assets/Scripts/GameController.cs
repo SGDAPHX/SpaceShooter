2fcce1a7db6a058677eee0a53b00c63388354ad8
Redeamed <kdavis928@yahoo.com>
2014-12-10 14:53:32 -0700

    Chapter12 Game Controller Completed
    Finished up through the Game Controller Chapter.  Minor error with
    screen bounds that I need to fix but otherwise the ideas are all there.
    Off to work now.

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazards;
+	public Vector3 spawnValue;
+
+	void Start()
+	{
+		SpawnWaves ();
+	}
+	void SpawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x,spawnValue.x),0,spawnValue.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazards, spawnPosition, spawnRotation);
+	}
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+
+	public float xMin, xMax, zMin, zMax;
+}
+public class PlayerController : MonoBehaviour {
+
+	public Vector2 speed = new Vector2 (1, 1);
+	public float tilt;
+	public Boundary bound;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate = 0.5f;
+	private float nextFire = 0;
+	void FixedUpdate()
+	{
+		float moveH = speed.x * Input.GetAxis ("Horizontal");
+		float moveV = speed.y * Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveH, 0, moveV);
+		rigidbody.velocity = movement;
+
+		rigidbody.position = new Vector3 
+			(
+				Mathf.Clamp(rigidbody.position.x,bound.xMin,bound.xMax),
+				0,
+				Mathf.Clamp(rigidbody.position.z,bound.zMin, bound.zMax));
+		rigidbody.rotation = Quaternion.Euler (0, 0, rigidbody.velocity.x * -tilt);
+	}
+
+	void Update()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			GameObject clone = Instantiate (shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+		}
+	}
+}
