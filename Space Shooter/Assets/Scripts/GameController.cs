6b9d8bbe97d94cfae076705840eac077933225e2
Redeamed <kdavis928@yahoo.com>
2014-12-13 22:02:44 -0700

    Invincibility temporarily
    Added timed invincibility after damage is taken.  as well as a few
    PlayerAI optimization tweaks.

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -5,15 +5,38 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazards;
 	public Vector3 spawnValue;
+	public int numHostiles = 10;
+	public int hosIncreaseRate = 1;
+	public float spawnWait   = 1.0f;
+	public float spawnWaitMin   = 1.0f;
+	public int spawnWaitRateDecrease   = 3;
+
+	public float WaveWait    = 10f;
 
 	void Start()
 	{
-		SpawnWaves ();
+		StartCoroutine ("SpawnWaves");
 	}
-	void SpawnWaves()
+	IEnumerator SpawnWaves()
 	{
-		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x,spawnValue.x),0,spawnValue.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazards, spawnPosition, spawnRotation);
+		for (int i = 0; i < numHostiles; ++i) {
+			yield return new WaitForSeconds (spawnWait);
+			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValue.x, spawnValue.x), 0, spawnValue.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (hazards, spawnPosition, spawnRotation);
+		}
+		
+		yield return new WaitForSeconds (WaveWait);
+		// increase diificulty over time
+		if (spawnWait > spawnWaitMin && numHostiles % spawnWaitRateDecrease == 0) 
+		{
+			//decrease time between spawns
+			spawnWait -= 0.1f;
+			if(spawnWait < spawnWaitMin){spawnWait = spawnWaitMin;}
+		}
+		//increase entities spawned per wave
+		numHostiles += hosIncreaseRate;
+		////////restart coRoutine
+		StartCoroutine ("SpawnWaves");
 	}
 }
--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,11 @@ public class Boundary
 }
 public class PlayerController : MonoBehaviour {
 
+	public float maxEnergy = 10;
+	public float currEnergy;
+
+	private float boostCost = 5;
+
 	public Vector2 speed = new Vector2 (1, 1);
 	public float tilt;
 	public Boundary bound;
@@ -17,10 +22,33 @@ public class PlayerController : MonoBehaviour {
 	public Transform shotSpawn;
 	public float fireRate = 0.5f;
 	private float nextFire = 0;
+
+	void Start()
+	{
+		currEnergy = maxEnergy;
+	}
+
 	void FixedUpdate()
 	{
+
+		//regen energy
+		if (currEnergy < maxEnergy) 
+		{
+			currEnergy += Time.deltaTime;
+			if(currEnergy > maxEnergy)
+			{
+				currEnergy = maxEnergy;
+			}
+		}
+
 		float moveH = speed.x * Input.GetAxis ("Horizontal");
 		float moveV = speed.y * Input.GetAxis ("Vertical");
+		if (Input.GetKey (KeyCode.LeftShift) && currEnergy > boostCost) 
+		{
+			moveH 		*= 2;
+			currEnergy 	-= boostCost * Time.deltaTime;
+		}
+
 		Vector3 movement = new Vector3 (moveH, 0, moveV);
 		rigidbody.velocity = movement;
 
@@ -29,7 +57,9 @@ public class PlayerController : MonoBehaviour {
 				Mathf.Clamp(rigidbody.position.x,bound.xMin,bound.xMax),
 				0,
 				Mathf.Clamp(rigidbody.position.z,bound.zMin, bound.zMax));
-		rigidbody.rotation = Quaternion.Euler (0, 0, rigidbody.velocity.x * -tilt);
+				rigidbody.rotation = Quaternion.Euler (0, 0, rigidbody.velocity.x * -tilt
+		    );
+
 	}
 
 	void Update()
